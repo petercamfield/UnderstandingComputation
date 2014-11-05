fd5f863e7f88e25b55faf41874e2dea7f88fa18f
peter.camfield@scribestar.com <peter.camfield@scribestar.com>
2014-11-05 09:18:25 +0000

    First session - simple expressions

---
+++ simple.js
@@ -0,0 +1,39 @@
+function Number(value) {
+    this.value = value;
+    this.reducible = false;
+}
+Number.prototype.toString=function() {
+    return this.value.toString();
+};
+
+function Add(left, right) {
+    this.left = left;
+    this.right = right;
+    this.reducible = true;
+}
+Add.prototype.toString = function() {
+    return (this.left + " + " + this.right);
+};
+Add.prototype.reduce = function() {
+    if (this.left.reducible) return new Add(this.left.reduce(), this.right);
+    if (this.right.reducible) return new Add(this.left, this.right.reduce());
+    return new Number(this.left.value + this.right.value);
+};
+
+function Machine(expression) {
+    this.expression = expression;
+}
+Machine.prototype.step = function() {
+    this.expression = this.expression.reduce();
+};
+Machine.prototype.run = function() {
+    while(this.expression.reducible===true) {
+        console.log(this.expression, this.expression.toString());
+        this.step();
+    }
+    console.log(this.expression, this.expression.toString());
+};
+
+var expression = new Add(new Number(1), new Add(new Number(2), new Number(3)));
+var machine = new Machine(expression);
+machine.run();
