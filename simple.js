deae508bbf0375c9ac2c4d806e01a00a8dee01f7
peter.camfield@scribestar.com <peter.camfield@scribestar.com>
2014-11-11 13:11:05 +0000

    Added multiplication

--- simple.js
+++ simple.js
@@ -2,7 +2,7 @@ function Number(value) {
     this.value = value;
     this.reducible = false;
 }
-Number.prototype.toString=function() {
+Number.prototype.toString = function () {
     return this.value.toString();
 };
 
@@ -11,23 +11,37 @@ function Add(left, right) {
     this.right = right;
     this.reducible = true;
 }
-Add.prototype.toString = function() {
+Add.prototype.toString = function () {
     return ("(" + this.left + " + " + this.right + ")");
 };
-Add.prototype.reduce = function() {
+Add.prototype.reduce = function () {
     if (this.left.reducible) return new Add(this.left.reduce(), this.right);
     if (this.right.reducible) return new Add(this.left, this.right.reduce());
     return new Number(this.left.value + this.right.value);
 };
 
+function Multiply(left, right) {
+    this.left = left;
+    this.right = right;
+    this.reducible = true;
+}
+Multiply.prototype.toString = function () {
+    return ("(" + this.left + " x " + this.right + ")");
+};
+Multiply.prototype.reduce = function () {
+    if (this.left.reducible) return new Multiply(this.left.reduce(), this.right);
+    if (this.right.reducible) return new Multiply(this.left, this.right.reduce());
+    return new Number(this.left.value * this.right.value);
+};
+
 function Machine(expression) {
     this.expression = expression;
 }
-Machine.prototype.step = function() {
+Machine.prototype.step = function () {
     this.expression = this.expression.reduce();
 };
-Machine.prototype.run = function() {
-    while(this.expression.reducible===true) {
+Machine.prototype.run = function () {
+    while (this.expression.reducible === true) {
         console.log(this.expression, this.expression.toString());
         this.step();
     }
