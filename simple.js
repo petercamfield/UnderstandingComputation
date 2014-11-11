f04326e377ca55044046f77301ead51a8c2a3b98
nickm-scribestar <nick.meldrum@scribestar.com>
2014-11-11 15:33:37 +0000

    course 2: variables

--- simple.js
+++ simple.js
@@ -6,6 +6,31 @@ Number.prototype.toString = function () {
     return this.value.toString();
 };
 
+function Bool(value) {
+    this.value = value;
+    this.reducible = false;
+}
+
+Bool.prototype.toString = function() {
+    return this.value.toString();
+}
+
+function Bigger(left, right) {
+    this.left = left;
+    this.right =right;
+    this.reducible = true;
+}
+
+Bigger.prototype.toString = function(){
+    return "(" + this.left + " > " + this.right + ")";
+};
+
+Bigger.prototype.reduce = function(environment){
+    if (this.left.reducible) return new Bigger(this.left.reduce(environment), this.right);
+    if (this.right.reducible) return new Bigger(this.left, this.right.reduce(environment));
+    return new Bool(this.left.value > this.right.value);
+};
+
 function Add(left, right) {
     this.left = left;
     this.right = right;
@@ -26,7 +51,7 @@ function Multiply(left, right) {
     this.reducible = true;
 }
 Multiply.prototype.toString = function () {
-    return ("(" + this.left + " x " + this.right + ")");
+    return "(" + this.left + " x " + this.right + ")";
 };
 Multiply.prototype.reduce = function () {
     if (this.left.reducible) return new Multiply(this.left.reduce(), this.right);
@@ -34,11 +59,25 @@ Multiply.prototype.reduce = function () {
     return new Number(this.left.value * this.right.value);
 };
 
-function Machine(expression) {
+function Variable (name) {
+    this.name = name;
+    this.reducible = true;
+}
+
+Variable.prototype.toString = function () {
+    return this.name.toString();
+};
+
+Variable.prototype.reduce = function(environment) {
+    return environment[this.name];
+};
+
+function Machine(expression, environment) {
     this.expression = expression;
+    this.environment = environment;
 }
 Machine.prototype.step = function () {
-    this.expression = this.expression.reduce();
+    this.expression = this.expression.reduce(this.environment);
 };
 Machine.prototype.run = function () {
     console.group(" --Run-- ");
@@ -49,4 +88,6 @@ Machine.prototype.run = function () {
     console.log(this.expression.toString(), this.expression);
     console.groupEnd();
 };
+
+
 
